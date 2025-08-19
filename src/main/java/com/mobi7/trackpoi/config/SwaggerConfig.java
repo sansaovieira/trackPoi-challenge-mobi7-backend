281b458383d47aec94ca728b980aa552d5888367
sansaovieira <vieirasansao42@gmail.com>
2025-08-19 13:27:35 -0300

    refactor: Corrigida padronização de parâmetros e tipos no Swagger

--- src/main/java/com/mobi7/trackpoi/config/SwaggerConfig.java
+++ src/main/java/com/mobi7/trackpoi/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,52 +19,29 @@ public class SwaggerConfig {
 	}
 
 	@Bean
-	public OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
-		return openApi -> openApi.getPaths().forEach((path, pathItem) -> {
+	public GroupedOpenApi trackpoiApi() {
+		return GroupedOpenApi.builder().group("trackpoi").packagesToScan("com.mobi7.trackpoi.controller").build();
+	}
 
-			pathItem.readOperations().forEach(operation -> {
-				ApiResponses apiResponses = operation.getResponses();
-
-				switch (path) {
-				case "/api/dwells":
-					apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
-					apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
-					apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
-					apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
-					break;
-
-				case "/api/dwells/plate/{plate}":
-					apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
-					apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
-					apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
-					break;
-
-				case "/api/dwell-engine/calculate":
-					apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
-					apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
-					apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
-					break;
-
-				case "/api/pois":
-					apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
-					apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
-					apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
-					apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
-					break;
-
-				case "/api/vehicles/plates":
-				case "/api/vehicles/positions":
-					apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
-					apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
-					apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
-					apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
-					break;
-
-				default:
-					break;
-				}
+	@Bean
+	public OpenApiCustomizer globalResponsesCustomizer() {
+		return openApi -> {
+			openApi.getPaths().forEach((path, pathItem) -> {
+				pathItem.readOperations().forEach(operation -> {
+					ApiResponses responses = operation.getResponses();
+					if (operation.getTags().contains("Dwell Events") || operation.getTags().contains("Dwell Engine")
+							|| operation.getTags().contains("POIs") || operation.getTags().contains("Veículos")) {
+						responses.addApiResponse("200", createApiResponse("Sucesso!"));
+						responses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+						responses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+						responses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+						responses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+						responses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+						responses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+					}
+				});
 			});
-		});
+		};
 	}
 
 	private ApiResponse createApiResponse(String message) {
